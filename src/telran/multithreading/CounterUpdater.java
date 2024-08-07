a5aefb15ba2e92a1bfc27c8a50efaaa66cbe5f9e
user.emial <emil.mehraliyev003@gmail.com>
2024-08-07 17:29:08 +0300

    ClassWork and selfPractice 43

---
+++ src/telran/multithreading/CounterUpdater.java
@@ -0,0 +1,30 @@
+package telran.multithreading;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+public class CounterUpdater extends Thread {
+	private static AtomicLong counter = new AtomicLong(0);
+	private int nRuns;
+
+	public CounterUpdater(int nRuns) {
+		this.nRuns = nRuns;
+	}
+
+	@Override
+	public void run() {
+		for (int i = 0; i < nRuns; i++) {
+			counterIncrement();
+		}
+	}
+
+	 private void counterIncrement() {
+		counter.getAndIncrement();
+
+	}
+
+	public static long getCounter() {
+
+		return counter.get();
+	}
+
+}
